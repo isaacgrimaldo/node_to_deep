e2aff45ff09649bfeb406dc4c4e88dd77e8fd3c1
Isaac Grimaldo <isaacgrimaldo432@gmail.com>
2021-10-12 00:40:45 -0500

    filesystem -> finished the method fs.readdir

---
+++ filesystem/readdir.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const path = require('path');
+
+const dir = path.join(__dirname,'templates');
+const dirBuf = Buffer.from(dir, 'utf-8');
+
+//sync way
+const files =  fs.readdirSync(dirBuf ,{encoding:'utf-8'});
+console.log(files);
+
+//async way
+fs.readdir(dirBuf ,{encoding:'utf-8'},(err, files) => {
+    if (err) throw new Error(err);
+
+    console.log(files);
+})
