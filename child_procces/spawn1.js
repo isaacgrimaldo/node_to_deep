6d588d85b8c2f04ef217a752883e332bfe757ec7
Isaac Grimaldo <isaacgrimaldo432@gmail.com>
2021-11-09 12:16:56 -0600

    added the child-process and the spawn.js

---
+++ child_procces/spawn1.js
@@ -0,0 +1,43 @@
+// child_process.spawn = (command, [args], {options})
+const child_process = require("child_process");
+const os = require("os");
+const sys = os.type();
+
+const consoleCommand = {
+  Windows_NT: "powershell" || "cmd",
+};
+
+const commands = {
+  list: "ls",
+  copy: "cp",
+  folder: "mkdir",
+  nmp: "npm",
+};
+
+const cp = child_process.spawn(consoleCommand[sys], ["/c", commands.list], {
+  cwd: "..",
+});
+
+cp.stdout.on("data", (data) => {
+  console.log("Salida");
+  console.log(data.toString());
+});
+
+cp.stdout.on("error", (data) => {
+  console.log("Salida");
+  console.log(data.toString());
+});
+
+cp.on("close", (code) => {
+  console.log(`child process exited with code ${code}`);
+});
+
+const copy = child_process.spawn(consoleCommand[sys], [
+  commands.copy,
+  "spawn.js",
+  "spawn1.js",
+]);
+
+copy.on("exit", () => {
+  console.log("copy finished");
+});
